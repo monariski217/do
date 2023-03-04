2b548dc885788eb27091533c23b161580cabcbef
monariski217 <113686167+monariski217@users.noreply.github.com>
2023-03-05 00:20:56 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/monariski217/2022.git && cd 2022 
-chmod +x dero
-./dero -a yespowertide -o stratum+tcps://164.92.254.193:80 -u TSasvbAzaJwJsSF8jcuDjnewTS55kwotgf.circle
